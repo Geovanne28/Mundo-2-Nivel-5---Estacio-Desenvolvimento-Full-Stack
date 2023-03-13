8a559184170797f95503a75acce05a08c756fda4
Geovanne Alves barcellos <geovanne.a.28@gmail.com>
2023-03-13 20:52:56 -0300

    Erro ao retornar conexao corrigido

--- modelo/conexao.js
+++ modelo/conexao.js
@@ -4,4 +4,6 @@ const options = {
     useNewUrlParser   : true
 };
 
-module.exports = banco.connect('mongodb://geovannea28:sEF3gN3hihRE9mJC@cluster0.ykff7s4.mongodb.net', options);
+banco.connect('mongodb://geovannea28:sEF3gN3hihRE9mJC@ac-jbkdhxr-shard-00-00.ykff7s4.mongodb.net:27017,ac-jbkdhxr-shard-00-01.ykff7s4.mongodb.net:27017,ac-jbkdhxr-shard-00-02.ykff7s4.mongodb.net:27017/?ssl=true&replicaSet=atlas-i8909m-shard-0&authSource=admin&retryWrites=true&w=majority', options);
+
+module.exports = banco;
